d6b1241b4f5ce2c05f3d523558e6b4e7e7809fb7
Randy Smukulis <randy.smukulis@ericsson.com>
2016-04-19 21:04:06 +0100

    initial youtube download code

--- ytgify/Program.cs
+++ ytgify/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ytgify
 {
+    using System.IO;
+
+    using YoutubeExtractor;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var link = "https://www.youtube.com/watch?v=2a4gyJsY0mc";
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(link);
+
+            VideoInfo video = videoInfos.First(info => info.VideoType == VideoType.Mp4);
+
+            if (video.RequiresDecryption)
+            {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+
+            var fname = video.Title;
+            var badChars = Path.GetInvalidFileNameChars().Where(c => fname.Contains(c));
+            
+            foreach (var badChar in badChars)
+            {
+                fname = fname.Replace(badChar, '_');
+            }
+
+            var videoDownloader = new VideoDownloader(video, fname + video.VideoExtension);
+
+            videoDownloader.Execute();
         }
     }
 }
