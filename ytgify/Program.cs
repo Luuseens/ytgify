1d94053c8b63c4d89e03fe66244dbcfeb21d4cae
Randy Smukulis <randy.smukulis@ericsson.com>
2016-04-19 21:40:48 +0100

    added frame extraction capability

--- ytgify/Program.cs
+++ ytgify/Program.cs
@@ -3,7 +3,11 @@ using System.Linq;
 
 namespace ytgify
 {
+    using System;
+    using System.Drawing;
     using System.IO;
+
+    using AForge.Video.FFMPEG;
 
     using YoutubeExtractor;
 
@@ -29,9 +33,29 @@ namespace ytgify
                 fname = fname.Replace(badChar, '_');
             }
 
-            var videoDownloader = new VideoDownloader(video, fname + video.VideoExtension);
+            var videoDownloader = new VideoDownloader(video, Path.Combine(Environment.CurrentDirectory, fname + video.VideoExtension));
 
             videoDownloader.Execute();
+            
+            
+
+            var reader = new VideoFileReader();
+            reader.Open(Path.Combine(Environment.CurrentDirectory, fname + video.VideoExtension));
+
+            for (int i = 0; i < 1500; i++)
+            {
+                Bitmap videoFrame = reader.ReadVideoFrame();
+                videoFrame.Save(Path.Combine(Environment.CurrentDirectory, "screenie" + i + ".png"));
+                // process the frame somehow
+                // ...
+
+                // dispose the frame when it is no longer required
+                videoFrame.Dispose();
+            }
+
+            reader.Close();
+            
+
         }
     }
 }
